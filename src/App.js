42859863d0d61f326fece63ca452d3c1cf818e4f
David Dasenbrook <david.dasenbrook@gmail.com>
2018-11-17 19:55:22 +0100

    implement creating, updating and deleting articles

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { Admin, Resource } from 'react-admin';
 import jsonServerRestClient from 'ra-data-json-server';
 
-import { ArticleList } from './articles';
+import { ArticleList, ArticleEdit, ArticleCreate } from './articles';
 import { authorizedHttpClient, authProvider } from './authClient';
 
 const App = () => (
     <Admin authProvider={authProvider} title="Jalkapallo" dataProvider={jsonServerRestClient('http://localhost:3001', authorizedHttpClient)}>
-        <Resource name="articles" list={ArticleList} />
+        <Resource name="articles" list={ArticleList} edit={ArticleEdit} create={ArticleCreate}/>
     </Admin>
 );
 
--- src/articles.js
+++ src/articles.js
@@ -1,12 +1,31 @@
 import React from 'react';
-import { List, Datagrid, TextField } from 'react-admin';
+import { List, Edit, Datagrid, TextField, SimpleForm, DisabledInput, TextInput, LongTextInput } from 'react-admin';
 
-export const ArticleList = (props) => (
+export const ArticleList = props => (
     <List {...props}>
-        <Datagrid>
+        <Datagrid rowClick="edit">
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
         </Datagrid>
     </List>
+);
+
+export const ArticleEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <DisabledInput source="id" />
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const ArticleCreate = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="title" />
+            <LongTextInput source="body" />
+        </SimpleForm>
+    </Edit>
 );
